748b7789598507f950e2ba6d2d874b430ede04c2
farhaniupr <farhaniupr@gmail.com>
2022-09-23 17:24:58 +0700

    done challenge

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import ContactForm from "./components/ContactForm";
+import Contact from "./components/Contact";
+import { Grid } from "@mui/material";
+import contactsJSON from "./data/contacts.json";
+import { useState } from "react";
 
 // Uncomment untuk memuat daftar kontak
 // import contactsJSON from './data/contacts.json';
@@ -10,8 +16,28 @@ const App = () => {
   // Masukkan contacts yang sudah didapat dalam JSON sebagai initial state
   // Buatlah handler untuk menambahkan kontak baru yang akan dikirim ke ContactForm
 
+  const [data, setData] = useState(contactsJSON);
+
+  const addData = (value) => {
+    const newData = [...data, value];
+    setData(newData);
+  };
+
   return (
     <div className="App">
+      <Header></Header>
+      <Grid style={{ marginTop: "50px" }} container spacing={2} columns={12}>
+        <Grid item xs={6} md={6}>
+          <div style={{ marginLeft: "100px", marginRight: "100px", justifyContent: "center" }}>
+            <ContactForm addData={addData}></ContactForm>
+          </div>
+        </Grid>
+        <Grid item xs={6} md={6}>
+          <div style={{ marginLeft: "100px", marginRight: "100px", justifyContent: "center" }}>
+            <Contact data={data}></Contact>
+          </div>
+        </Grid>
+      </Grid>
     </div>
   );
 };
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,13 +3,96 @@
 // dan Card
 // https://mui.com/material-ui/react-card/#basic-card
 
-const ContactForm = () => {
-    // Form berisi name, phone, email, dan photo url
-    // Buatlah state newContact berupa objek sesuai dengan data yang ada
+import { TextField, Grid, Card, CardContent, Button } from "@mui/material";
+import * as R from "react";
+import {} from "./ContactForm.css";
 
-    return (
-        <> </>
-    );
-}
+const ContactForm = (action) => {
+  // Form berisi name, phone, email, dan photo url
+  // Buatlah state newContact berupa objek sesuai dengan data yang ada
+
+  const [email_input, setEmail] = R.useState("orang@orang.com");
+  const [phone_input, setPhone] = R.useState("087771711777");
+  const [name_input, setName] = R.useState("Orang");
+  const [photo_input, setPhoto] = R.useState("http://placekitten.com/100");
+
+  const [newContact, setnewContact] = R.useState({
+    name: name_input,
+    phone: phone_input,
+    email: email_input,
+    photo: photo_input
+  });
+
+  const AddNew = (event) => {
+    event.preventDefault();
+
+    action.addData({
+      name: name_input,
+      phone: phone_input,
+      email: email_input,
+      photo: phone_input
+    });
+  };
+
+  const changeinputtextName = (event) => {
+    event.preventDefault();
+    setName(event.target.value);
+  };
+
+  const changeinputtextPhoto = (event) => {
+    event.preventDefault();
+    setPhoto(event.target.value);
+  };
+
+  const changeinputtextPhone = (event) => {
+    event.preventDefault();
+    setPhone(event.target.value);
+  };
+
+  const changeinputtextEmail = (event) => {
+    event.preventDefault();
+    setEmail(event.target.value);
+  };
+
+  const card = (
+    <R.Fragment>
+      <CardContent>
+        <form onSubmit={AddNew}>
+          <Grid container direction="column" justifyContent="space-between" alignItems="flex-start">
+            <div>
+              <TextField
+                style={{ maxWidth: "100%" }}
+                id="name"
+                label="Name"
+                variant="outlined"
+                value={name_input}
+                onChange={changeinputtextName}
+                required
+              ></TextField>
+            </div>
+            <div>
+              <TextField id="phone" label="Phone" value={phone_input} onChange={changeinputtextPhone} required></TextField>
+            </div>
+            <div>
+              <TextField id="email" label="Email" value={email_input} onChange={changeinputtextEmail} required></TextField>
+            </div>
+            <div>
+              <TextField id="photo" label="Photo URL" value={photo_input} onChange={changeinputtextPhoto} required></TextField>
+            </div>
+          </Grid>
+          <Grid container direction="row" justifyContent="flex-start" alignItems="center">
+            <Button type="submit">Add New</Button>
+          </Grid>
+        </form>
+      </CardContent>
+    </R.Fragment>
+  );
+
+  return (
+    <>
+      <Card variant="outlined">{card}</Card>
+    </>
+  );
+};
 
 export default ContactForm;
