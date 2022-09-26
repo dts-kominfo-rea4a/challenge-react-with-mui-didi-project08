c0e64e0284ccf56321ef3ac6979f25ff5644ebf6
didi-project08 <91584217+didi-project08@users.noreply.github.com>
2022-09-26 12:38:53 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import "./App.css";
-import Header from "./components/Header";
-import ContactForm from "./components/ContactForm";
-import Contact from "./components/Contact";
-import { Grid } from "@mui/material";
-import contactsJSON from "./data/contacts.json";
-import { useState } from "react";
+import React, { useState } from 'react';
+import { styled } from '@mui/material/styles';
+import { Container, Grid, Paper, Box } from '@mui/material';
+import Header from './components/Header';
+import ContactForm from './components/ContactForm';
+import Contact from './components/Contact';
+import './App.css';
+import contactsJSON from './data/contacts.json';
 
 // Uncomment untuk memuat daftar kontak
 // import contactsJSON from './data/contacts.json';
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'left',
+  color: theme.palette.text.secondary,
+}));
+
 const App = () => {
   // Masukkan Header dan lakukan map untuk Contact ke dalam div App
   // untuk membuat daftar kontak bisa menggunakan MUI list
@@ -15,30 +24,34 @@ const App = () => {
 
   // Masukkan contacts yang sudah didapat dalam JSON sebagai initial state
   // Buatlah handler untuk menambahkan kontak baru yang akan dikirim ke ContactForm
-
-  const [data, setData] = useState(contactsJSON);
-
-  const addData = (value) => {
-    const newData = [...data, value];
-    setData(newData);
-  };
+  const [contacts, setContacts] = useState(contactsJSON);
+  const addContacts = (value) => {
+    const newContacts = [...contacts, value];
+    setContacts(newContacts);
+  }
 
   return (
-    <div className="App">
-      <Header></Header>
-      <Grid style={{ marginTop: "50px" }} container spacing={2} columns={12}>
-        <Grid item xs={6} md={6}>
-          <div style={{ marginLeft: "100px", marginRight: "100px", justifyContent: "center" }}>
-            <ContactForm addData={addData}></ContactForm>
-          </div>
-        </Grid>
-        <Grid item xs={6} md={6}>
-          <div style={{ marginLeft: "100px", marginRight: "100px", justifyContent: "center" }}>
-            <Contact data={data}></Contact>
-          </div>
-        </Grid>
-      </Grid>
-    </div>
+    <>
+      <div className="App">
+        <Container maxWidth="lg">
+          <Box sx={{ bgcolor: '#F0F8FF', height: '100vh' }}>
+            <Header />
+            <Grid sx={{ marginTop: '20px' }} container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+              <Grid item xs={6}>
+                <Item>
+                  <ContactForm fnAddContacts={addContacts} />
+                </Item>
+              </Grid>
+              <Grid item xs={6}>
+                <Item>
+                  <Contact contacts={contacts} />
+                </Item>
+              </Grid>
+            </Grid>
+          </Box>
+        </Container>
+      </div>
+    </>
   );
 };
 
