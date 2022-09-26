c9443bd7908a6bd7444d2ca490cfaf4ae4f3aa43
didi-project08 <91584217+didi-project08@users.noreply.github.com>
2022-09-26 12:52:22 +0700

    initial commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,43 +2,20 @@
 // atau langsung tambahkan dengan sx={{}}
 import React from 'react';
 import { Typography } from '@mui/material';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Divider from './Divider';
 
 // Header berisi judul dan subtitle
 // Judul: Call a Friend
 // Subtitle: Your friendly contact app
-const theme = createTheme({
-  typography: {
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
-    subtitle1: {
-      fontSize: 17,
-    },
-  },
-});
 
 const Header = () => {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <Typography variant="h3" sx={{marginTop: '10px'}}>
-          Call a Friend
-        </Typography>
-        <Typography variant="subtitle1" sx={{marginTop: '10px'}}>
-          <Divider>your friendly contact app</Divider>
-        </Typography>
-      </ThemeProvider>
+      <Typography variant="h3" sx={{marginTop: '10px'}}>
+        Call a Friend
+      </Typography>
+      <Typography variant="subtitle1" sx={{marginTop: '10px'}}>
+        your friendly contact app
+      </Typography>
     </>
   );
 };
